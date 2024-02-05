e2c9d923dcd8d35cc4221de76b202c99fe38fe55
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-02-05 23:16:32 +0530

    implemented form data

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+const App = () => {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // function changeFirstHandler(event) {
+  //   console.log("printing first name");
+  //   console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+  // function changeLastHandler(event) {
+  //   console.log("printing last name");
+  //   console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isVisible: true,
+  });
+
+  console.log(formData);
+
+  function changeHandler(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  return (
+    <div className="App">
+      <form>
+        <input
+          type="text"
+          placeholder="first name"
+          onChange={changeHandler}
+          name="firstName"
+          value={formData.firstName}
+        />
+
+        <br />
+
+        <input
+          type="text"
+          placeholder="last name"
+          onChange={changeHandler}
+          name="lastName"
+          value={formData.lastName}
+        />
+
+        <br />
+
+        <input
+          type="text"
+          placeholder="email address"
+          onChange={changeHandler}
+          name="email"
+          value={formData.email}
+        />
+
+        <br />
+        <textarea
+          placeholder="enter your comments here"
+          onChange={changeHandler}
+          name="comments"
+          value={formData.comments}
+        />
+        <br />
+
+        <input
+          type="checkbox"
+          onChange={changeHandler}
+          name="isVisible"
+          id="isVisible"
+          checked={formData.isVisible}
+        />
+        <label htmlFor="isVisible">Am I visible</label>
+      </form>
+    </div>
+  );
+};
+
+export default App;
